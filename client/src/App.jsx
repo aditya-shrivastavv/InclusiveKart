72033cacc9cb95379da56e3323c6451e29d65d43
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-03-14 11:01:51 +0530

    home section added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 
 import Navbar from "./components/Header/Navbar";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Home />
       </div>
     </BrowserRouter>
   );
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <section>
+      <div className="container mx-auto grid grid-cols-5">
+        <div className="col-span-1 h-28 bg-green-400"></div>
+        <div className="col-span-4 h-28 bg-blue-500"></div>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
